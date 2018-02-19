01b1509fe17460a819d7a715f0d56e1c02da1c2e
Honorio Vega <hvega@csumb.edu>
2018-02-19 15:04:05 -0800

    Associated username and password fields to outlet in view controller class

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
