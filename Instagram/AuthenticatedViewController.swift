7b9e15ef8834d59ee2231c73e8e897da4f9efa8c
Honorio Vega <hvega@csumb.edu>
2018-02-21 15:47:27 -0800

    4 - The current signed in user is persisted across app restarts

--- Instagram/AuthenticatedViewController.swift
+++ Instagram/AuthenticatedViewController.swift
@@ -9,18 +9,34 @@
 import UIKit
 import Parse
 
-class AuthenticatedViewController: UIViewController {
+class AuthenticatedViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
 
     @IBOutlet weak var greetingMessageView: UILabel!
     override func viewDidLoad() {
+        
         
         if PFUser.current() != nil {
             let username = PFUser.current()?.username as! String
             greetingMessageView.text = username
         }
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +48,26 @@ class AuthenticatedViewController: UIViewController {
     @IBAction func onLogout(_ sender: Any) {
          NotificationCenter.default.post(name: NSNotification.Name("didLogout"), object: nil)
     }
-    
+    @IBAction func onCompose(_ sender: UIBarButtonItem) {
+        self.performSegue(withIdentifier: "composeSegue", sender: nil)
 
+    }
+    
+//    @IBAction func onAddImage(_ sender: UIBarButtonItem) {
+//        
+//        // Instantiate a UIImagePickerController
+//        let vc = UIImagePickerController()
+//        vc.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+//        vc.allowsEditing = true
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            print("Camera is available 📸")
+//            vc.sourceType = .camera
+//        } else {
+//            print("Camera 🚫 available so we will use photo library instead")
+//            vc.sourceType = .photoLibrary
+//        }
+//        self.present(vc, animated: true, completion: nil)
+//    }
+    
 }
